9fe382a73484772c8a1928760a0cb910dd5bcbcb
Ruben <rubencidep@gmail.com>
2025-01-17 12:27:38 +0100

    Case 1 resolved

---
+++ ImageSense/__init__.py
@@ -0,0 +1,3 @@
+from face_recob_lib import *
+
+blur_faces(r'..\imagenes\vengadores.png',r'..\json\vengadores_rkg.json')
---
+++ ImageSense/face_recob_lib.py
@@ -0,0 +1,59 @@
+# Importar libreria cv2
+# Instalar cv2  pip3 install opencv-python
+import cv2
+import os
+import json
+
+
+
+# Función para guardar una imagen en la carpeta img
+def save_image(ruta, text, img):
+    # Usar os.path.splitext() para separar el nombre y la extensión
+    nombre, extension = os.path.splitext(ruta)
+    # Crear la nueva ruta con el nombre modificado y la misma extensión
+    nueva_imagen = nombre + text + extension
+    # La guardamos en memoria en un fichero distinto y devolvemos la ruta
+    cv2.imwrite(nueva_imagen, img)
+
+
+###########################################################################################
+# Caso 1
+# Se desea diseñar una aplicación que aplique difuminado a los rostros de una imagen dada,
+# generando una nueva imagen, a través de la información extraída de un json usando AWS Rekognition.
+###########################################################################################
+
+def blur_faces(ruta_img,json_path):
+
+    img = cv2.imread(ruta_img)
+    # Obtener las dimensiones de la imagen
+    image_height, image_width, _ = img.shape
+
+    with open(json_path, "r") as file:
+        data = json.load(file)
+
+    # Verificar si existen detalles de caras
+    if "FaceDetails" not in data or not data["FaceDetails"]:
+        raise ValueError("El archivo JSON no contiene información de las caras.")
+
+    # Procesar cada cara detectada en el JSON
+    for face in data["FaceDetails"]:
+        bounding_box = face.get("BoundingBox", {})
+        if not bounding_box:
+            continue
+
+        # Calcular las coordenadas absolutas de la cara
+        x = int(bounding_box["Left"] * image_width)
+        y = int(bounding_box["Top"] * image_height)
+        width = int(bounding_box["Width"] * image_width)
+        height = int(bounding_box["Height"] * image_height)
+
+        # Recortar el área de la cara
+        face_region = img[y:y + height, x:x + width]
+
+        # Aplicar un filtro Gaussiano para difuminar la región
+        blurred_face = cv2.GaussianBlur(face_region, (51, 51), 30)
+
+        # Reemplazar la región original con la difuminada
+        img[y:y + height, x:x + width] = blurred_face
+
+    save_image(ruta_img,'_Dif',img)
